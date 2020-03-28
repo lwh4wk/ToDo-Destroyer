e3c444a3138519877c4c2922357729cb03ee8daf
Logan Hylton <37708431+lwh4wk@users.noreply.github.com>
2020-03-28 12:33:58 -0400

    ready to deploy

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 
-app.use(express.static('static_files'));
-
-app.listen(3000, () => {
+app.listen(process.env.port || 3000, () => {
     console.log('Server started at http://localhost:3000/');
 });
 
