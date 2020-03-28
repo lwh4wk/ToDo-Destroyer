0af8675d8c7ac359d0b652ed7bb5f29880439534
Logan Hylton <37708431+lwh4wk@users.noreply.github.com>
2020-03-28 12:44:35 -0400

    deploy

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
