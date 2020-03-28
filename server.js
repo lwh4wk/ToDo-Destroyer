8facb1eac1dc46cbb30b7d2946642b39942421c0
Logan Hylton <37708431+lwh4wk@users.noreply.github.com>
2020-03-28 12:38:01 -0400

    empty

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-
-app.listen(process.env.port || 3000, () => {
-    console.log('Server started at http://localhost:3000/');
-});
-
