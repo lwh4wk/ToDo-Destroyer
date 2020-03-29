28cc0b8ac7d7df210d5e3af6a64b016837d05b54
Logan Hylton <37708431+lwh4wk@users.noreply.github.com>
2020-03-29 02:47:03 -0400

    Update xpbar.php

--- xpbar.php
+++ xpbar.php
@@ -6,7 +6,8 @@
     $xp = $row[0];
     $level = $row[1];
 ?>
-<h5>Current xp: <?php echo $xp; ?>, <?php echo 1000 - $xp ?> xp until you reach the next level.</h5>
+<h5>Current level: <?php echo $level; ?></h5>
+<h5>Current xp: <?php echo $xp; ?>, <?php echo 5000 - $xp ?> xp until you reach the next level.</h5>
 <div class="progress">
-    <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $xp/10; ?>%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo $xp/10; ?>%</div>
+    <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $xp/50; ?>%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo $xp/50; ?>%</div>
 </div>
