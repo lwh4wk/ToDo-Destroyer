dbc0d0f7ed82a39a5499b149e5f7f6e3b10b4ec9
Cate <cpbx29@gmail.com>
2020-03-28 22:44:59 -0400

    Crappp

--- index.php
+++ index.php
@@ -25,8 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
@@ -248,7 +247,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </table>
             </section>
             <section id="workspace" hidden>
-                <p>Hello</p>
 
             </section>
         </main>
